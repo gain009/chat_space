739b8c78eb13915b034770820d6a812b77c8d278
gain009 <63448529+gain009@users.noreply.github.com>
2020-05-18 19:55:22 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,20 @@ ActiveRecord::Schema.define(version: 20200513063707) do
     t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
+<<<<<<< Updated upstream
+=======
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.string   "image"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  end
+
+>>>>>>> Stashed changes
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
@@ -44,4 +58,9 @@ ActiveRecord::Schema.define(version: 20200513063707) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+<<<<<<< Updated upstream
+=======
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
+>>>>>>> Stashed changes
 end
